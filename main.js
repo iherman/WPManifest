2b155bf3039807de8e2ce5153303a6a08395ff82
Ivan Herman <ivan@ivan-herman.net>
2018-03-21 16:21:57 +0100

    Has now a complete skeleton and the tests are also local.

--- main.js
+++ main.js
@@ -1,24 +1,100 @@
 #!/usr/bin/env node
 "use strict";
 
-const nav = require("./lib/nav");
+const nav                       = require("./lib/nav");
+const {fetch_html, fetch_json}  = require("./lib/io");
+const url                       = require("url");
+const _                         = require("underscore");
 
-let manifest = {
-    // "reading_order" : ["http://www.lo.pikula", "http://www.cin.tanyer"],
-    "resources" : [{
-        "href": "http://localhost:8001/LocalData/2018/manifest/nav.html",
-        "rel":  ["contents"]
-    }]
-}
 
-async function main() {
+/**
+ * Main processing steps to obtain the manifest
+ *
+ * @param {string} document_url - The URL of the HTML start up file.
+ * @returns {Object} - JS representation of the manifest
+ */
+async function obtain_manifest(document_url) {
     try {
-        manifest.reading_order = await nav.getReadingOrder(manifest);
-        console.log(manifest);
+        // This is not part of the official algorithm: getting the DOM of the start up file
+        let top_level = await fetch_html(document_url);
+        // Get the first <link> in document order that has the publication rel value:
+        let manifest_link = top_level.window.document.querySelector('link[rel*="publication"]');
+        let manifest_url  = url.resolve(document_url, manifest_link.href);
+
+        // Fetching the manifest content as text
+        let text     = await fetch_json(manifest_url);
+        let manifest = await process_manifest(text, manifest_url, document_url);
+        return manifest;
     } catch(err) {
-        console.error(err);
+        console.warn(`Error occured in ${url}: ${err}`);
+        return {};
     }
 }
 
+/**
+ * Process the manifest
+ *
+ * @param {string} text - the textual version of the manifest json file
+ * @param {string} manifest_url - the (absolute) URL of the manifest
+ * @param {string} document_url - the (absolute) URL of the startin page
+ * @returns {Object} - JS representation of the manifest
+ */
+async function process_manifest(text, manifest_url, document_url) {
+    // Parse the text into json
+    let json = {};
+    try {
+        json = JSON.parse(text);
+    } catch(err) {
+        console.warn(`Syntax error in the manifest at ${manifest_url}: ${err}`)
+        return {};
+    }
+
+    // See if the json was an object or an array
+    if(_.isArray(json)) {
+        console.warn(`Manifest at ${manifest_url} is an array and not an object`)
+    }
+
+    // This is a fake entry right now. At some point it may include further processing
+    // on the manifest, possibly checks as well. Not yet part of the spec;
+    let manifest = convert(json, manifest_url);
+    // Get the default reading order
+    manifest.reading_order  = await nav.process_default_reading_order(manifest, manifest_url)
+
+    // That is it
+    return manifest;
+}
+
+/**
+ * Convert the json to the target manifest
+ *
+ * The changes are not (yet?) defined in the WP document. What does happen here is to turn all relative URL-s in links
+ * into absolute ones, with the document URL as the base.
+ *
+ * @param {Object} manifest - the unchecked and unconverted manifest
+ * @returns {Object} - the cleaned up manifest
+ */
+ function convert(json, manifest_url) {
+    // the strange idiom for deep cloning of an object...
+    let manifest = JSON.parse(JSON.stringify(json));
+
+    if(manifest.resources) {
+        let res = manifest.resources.map((obj) => {
+            obj.href = url.resolve(manifest_url, obj.href);
+            return obj;
+        })
+        manifest.resources = res;
+    }
+
+
+    return manifest
+ }
+
+
+async function main() {
+    let manifest = await obtain_manifest("http://localhost:8001/LocalData/github/WPManifest/test/entry.html");
+    console.log(JSON.stringify(manifest,null,2));
+}
+
+
 // Do it!
-main()
+main();
--- notes/default_reading_order.md
+++ notes/default_reading_order.md
@@ -4,3 +4,5 @@ Minor issues
 - after step 2/3: if navigation document link is null, the algorithm terminates (see above)
 - In step 10: if the document contains several 'nav' elements, only the first element is considered (in document order)
 - In 10.3 the base for the relative path is the URL of the navigation document URL
+
+- more generally: if the links in the json file are relative URL-s, they must be turned absolute with the manifest_url as base (or should it be the document_url?)
