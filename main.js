d5aa3284627c4d0b179a97d958859d87710b1b9d
Ivan Herman <ivan@ivan-herman.net>
2018-07-23 17:44:15 +0200

    First round...

--- main.js
+++ main.js
@@ -4,102 +4,93 @@
 
 const url                         = require('url');
 const _                           = require('underscore');
-const nav                         = require('./lib/nav');
+const path                        = require('path');
+
 const { fetch_html, fetch_json }  = require('./lib/io');
+const { obtain_manifest }         = require('./lib/top')
+
+
+/* ------------------------------------------------------------------------------ */
+//                    Local helper to turn local file names into URL-s...
+/* ------------------------------------------------------------------------------ */
+
+/* Localhost setups */
+const PORT    = '8001';
+const SCHEME  = 'http';
+const HOST    = 'localhost';
+
+// Filename for the server root
+const top               = '/Users/ivan/W3C/WWW';
+
+// github area as a real directory
+const slink_target      = '/Users/ivan/W3C/github/';
+
+// the symbolic link under root to github
+const slink_replacement = 'LocalData/github/';
 
 
 /**
- * Convert the json to the target manifest
+ * Helper function to test stuff locally; this is not necessary for production.
+ * The goal is to turn a possible local filename into a localhost URL, so that the
+ * rest of the library works only with proper URLs at every step.
  *
- * The changes are not (yet?) defined in the WP document.
- * What does happen here is to turn all relative URL-s in links
- * into absolute ones, with the document URL as the base.
+ * The setup is a bit tainted with my local setup that also has a symbolic link from
+ * my local web server's top to the github area (that is elsewhere on my disc),
+ * ie, a separate check is necessary to turn this symbolic link into the proper URL.
  *
- * @param {Object} manifest - the unchecked and unconverted manifest
- * @returns {Object} - the cleaned up manifest
+ * @param base: the file name of the node.js script running the tests, serving as a base for relative filenames
+ * @param arg: the argument denoting the test file
+ * @return: URL string
  */
-function convert(json, manifest_url) {
-    // the strange idiom for deep cloning of an object...
-    const manifest = JSON.parse(JSON.stringify(json));
-    if (manifest.resources) {
-        manifest.resources.forEach((obj) => {
-            obj.href = url.resolve(manifest_url, obj.href);
-        });
+function getURL(base, arg) {
+    if (arg === undefined) {
+        throw new Error('No argument...');
+    } else {
+        const parsed_arg = url.parse(arg);
+        if (parsed_arg.protocol) {
+            // The argument is a proper URL, nothing to do...
+            return arg;
+        }
+        // We've received a file system reference, which must be converted into an appropriate localhost...
+        // base is the name of the script itself as called by node.js
+        // We need the full path
+        const full_path = path.isAbsolute(arg) ? arg : path.join(path.dirname(base), arg);
+
+        if (full_path.startsWith(slink_target === false && full_path.startsWith(top) === false)) {
+            throw new Error('Cannot interpret file name...');
+        }
+        // This is a local file that will be served via localhost...
+        // Create the "path" for the test. This may depend on your local setups; mine is a bit
+        // complicated because the github directory is symbolically linked from a HTTP path,
+        // and this must be taken into account...
+        const retval = {
+            protocol : SCHEME,
+            hostname : HOST,
+            port     : PORT !== '' ? PORT : undefined,
+            pathname : full_path.startsWith(slink_target)
+                ? path.join(slink_replacement, full_path.slice(slink_target.length))
+                : full_path.slice(top.length)
+        };
+        return url.format(retval);
     }
-    if (manifest.reading_order) {
-        manifest.reading_order.forEach((obj) => {
-            obj.href = url.resolve(manifest_url, obj.href);
-        });
-    }
-    return manifest;
 }
 
 
-/**
- * Process the manifest
- *
- * @param {string} text - the textual version of the manifest json file
- * @param {string} manifest_url - the (absolute) URL of the manifest
- * @param {string} document_url - the (absolute) URL of the starting page
- * @returns {Object} - JS representation of the manifest
- */
-async function process_manifest(text, manifest_url, document_url) {
-    // Parse the text into json
-    let json = {};
-    try {
-        json = JSON.parse(text);
-    } catch (err) {
-        console.warn(`Syntax error in the manifest at ${manifest_url}: ${err}`);
-        return {};
-    }
-
-    // See if the json was an object or an array
-    if (_.isArray(json)) {
-        console.warn(`Manifest at ${manifest_url} is an array and not an object`);
-        return {};
-    }
-
-    // This is a fake entry right now. At some point it may include further processing
-    // on the manifest, possibly checks as well. Not yet part of the spec;
-    const manifest = convert(json, manifest_url);
-    // Get the default reading order
-    manifest.reading_order = await nav.process_default_reading_order(manifest);
-
-    // That is it
-    return manifest;
-}
-
-
-/**
- * Main processing steps to obtain the manifest
- *
- * @param {string} document_url - The URL of the HTML start up file.
- * @returns {Object} - JS representation of the manifest
- */
-async function obtain_manifest(document_url) {
-    try {
-        // This is not part of the official algorithm: getting the DOM of the start up file
-        const top_level = await fetch_html(document_url);
-        // Get the first <link> in document order that has the publication rel value:
-        const manifest_link = top_level.window.document.querySelector('link[rel*="publication"]');
-        const manifest_url  = url.resolve(document_url, manifest_link.href);
-
-        // Fetching the manifest content as text
-        const text     = await fetch_json(manifest_url);
-        const manifest = await process_manifest(text, manifest_url, document_url);
-        return manifest;
-    } catch (err) {
-        console.warn(`Error occured in ${url}: ${err}`);
-        return {};
-    }
-}
-
-/* --------------------------------------------------------------------------------------------------------------- */
+/* ------------------------------------------------------------------------------ */
+//                            Main entry point for testing
+/* ------------------------------------------------------------------------------ */
 
 // Set this to your local URL if you want to test locally...
 async function main() {
-    const manifest = await obtain_manifest('http://localhost:8001/LocalData/github/WPManifest/test/entry.html');
-    console.log(JSON.stringify(manifest, null, 2));
+    let final_url = "";
+    try {
+        final_url = getURL(process.argv[1], process.argv[2]);
+        const top_level = await fetch_html(final_url);
+        const manifest = await obtain_manifest(top_level);
+        console.log(JSON.stringify(manifest, null, 4));
+    } catch (err) {
+        console.warn(`Error occured when handling ${final_url}: ${err}`);
+    }
 }
 
 
