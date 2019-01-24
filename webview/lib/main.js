db67b4649d1fef6c3f71640e17941fcd21b7c17d
Ivan Herman <ivan@ivan-herman.net>
2019-01-24 17:35:35 +0100

    First working version...

--- webview/lib/main.js
+++ webview/lib/main.js
@@ -1,120 +1,171 @@
 'use strict';
 
-const { JSDOM }     = require('jsdom');
-const fetch         = require('node-fetch');
-const contentType   = require('content-type');
+/* eslint-env browser */
+/* eslint-disable no-alert */
+
 const {
-    check_url,
     obtain_manifest,
     Logger,
     fetch_json,
     fetch_html
- } = require('../../lib/utils');
+} = require('../../lib/utils');
 const { get_canonical_manifest } = require('../../lib/process');
 
-const json_content_type = 'application/json';
-const html_content_type = 'text/html';
-
-/* eslint-env browser */
 /**
- * Get Web resource via a fetch. There is a sanity (security) check on the URL to avoid possible security errors.
- *
- * @async
- * @param {string} resource_url - The URL of the resource to be fetched
- * @param {string} content_type - Expected content. Default is JSON (ie, application/json).
- *    Accepted values are HTML, and JSON (including the 'derivatives', ie, application/XXX+json)
- * @return {Promise} encapsulating the body of the resource. The appropriate parsing should be done by the caller
+ * Empty that various text fields when a new textual content is uploaded.
  */
-async function fetch_resource(resource_url) {
-    const base_content_type = (typ) => {
-        const [major, minor] = contentType.parse(typ).type.split('/');
-        const extras = minor.split('+');
-        return extras.length === 1 ? `${major}/${minor}` : `${major}/${extras[1]}`;
-    };
-
-    // If there is a problem, an exception is raised
-    return new Promise((resolve, reject) => {
-        // This is a real URL, whose content must be accessed via HTTP(S)
-        try {
-            // An exception is raised if the URL has security/sanity issues.
-            const final_url = check_url(resource_url);
-            fetch(final_url)
-                .then((response) => {
-                    if (response.ok) {
-                        const content_type = base_content_type(response.headers.get('content-type'));
-                        if (content_type === json_content_type || content_type === html_content_type) {
-                            const resource = response.text();
-                            resolve({ resource, content_type });
-                        } else {
-                            reject(new Error('HTML or JSON-LD are expected'));
-                        }
-                    } else {
-                        reject(new Error(`HTTP response ${response.status}: ${response.statusText}`));
-                    }
-                })
-                .catch((err) => {
-                    reject(new Error(`Problem accessing ${final_url}: ${err}`));
-                });
-        } catch (err) {
-            reject(err);
-        }
-    });
+function clean_up() {
+    document.getElementById('canonical_wpm').value = '';
+    document.getElementById('wpm_url').value = '';
+    document.getElementById('pep_url').value = '';
 }
 
+
+/**
+ * Fetch the JSON-LD manifest from the URL in the `wpm_url` input form, and display it in the
+ * relevant `wpm_holder` text box.
+ *
+ * @async
+ * @param {Event} e - Event structure (unused)
+ */
 async function get_wpm(e) {
     try {
         const wpm_url  = document.getElementById('wpm_url').value;
-        const wpm_holder = document.getElementById('wpm');
+        const wpm_holder = document.getElementById('wpm_holder');
+        const data = wpm_holder.dataset;
         const wpm = await fetch_json(wpm_url);
+        data.url = wpm_url;
         wpm_holder.value = wpm;
+        document.getElementById('canonical_wpm').value = '';
+        document.getElementById('pep_url').value = '';
     } catch (err) {
         console.error(`Error in get_pwm: ${err.message}`);
+        alert(`Error in get_pwm: ${err.message}`);
     }
 }
 
+
+/**
+ * Convert the content of the `wpm_holder` text box and put the result
+ * into the `canonical_wpm` text box.
+ *
+ * @param {Event} e - Event structure (unused)
+ */
 function convert(e) {
-    alert("Convert data from text box");
-    // const logger = new Logger();
-    // try {
-    //     const wpm = document.getElementById('wpm');
-    //     const data = wpm.dataset;
-
-    //     if (wpm.value !== '') {
-    //         const manifest_data = {
-    //             manifest_text : wpm.value,
-    //             dom           : data.dom === '' ? undefined : data.dom,
-    //             base          : data.base,
-    //             lang          : data.lang,
-    //             dir           : data.dir
-    //         };
-    //         console.log(data);
-    //         const canonical_wpm = document.getElementById('canonical_wpm');
-    //         manifest_data.dom = undefined;
-    //         const c_manifest_object = get_canonical_manifest(logger, manifest_data);
-
-    //         canonical_wpm.value = JSON.stringify(c_manifest_object, null, 4);
-    //     }
-    // } catch (err) {
-    //     console.log(`Error in canonicalization: ${err.message} in ${err.lineNumber}`);
-    // } finally {
-    //     console.log(logger.toString());
-    // }
+    const logger = new Logger();
+    try {
+        const wpm_holder = document.getElementById('wpm_holder');
+        if (wpm_holder.value !== '') {
+            const canonical_wpm = document.getElementById('canonical_wpm');
+            const data = wpm_holder.dataset;
+            const manifest_data = {
+                manifest_text : wpm_holder.value,
+                dom           : undefined,
+                base          : data.url,
+                lang          : '',
+                dir           : ''
+            };
+            const manifest_object = get_canonical_manifest(logger, manifest_data);
+            canonical_wpm.value = JSON.stringify(manifest_object, null, 4);
+        }
+    } catch (err) {
+        console.error(`Error in canonicalization: ${err.message} in ${err.lineNumber}`);
+        alert(`Error in canonicalization: ${err.message} in ${err.lineNumber}`);
+    } finally {
+        console.log(logger.toString());
+    }
 }
 
+
+/**
+ *
+ * Event handler to load an manifest from a local file.
+ *
+ * @param {Event} e
+ */
+// eslint-disable-next-line no-unused-vars
 function upload_wpm(e) {
-    alert("Upload a json file and put it into the text")
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('loadend', (ev) => {
+        const wpm_holder = document.getElementById('wpm_holder');
+        try {
+            JSON.parse(reader.result);
+            wpm_holder.value = reader.result;
+            wpm_holder.dataset.url = `file:///${file.name}/`;
+
+            document.getElementById('canonical_wpm').value = '';
+            document.getElementById('wpm_url').value = '';
+            document.getElementById('pep_url').value = '';
+        } catch (s) {
+            alert(`Invalid JSON file: ${s.toString()}`);
+        }
+    });
+    reader.readAsText(file);
 }
 
-function get_pep(e) {
-    alert("Fetch PEP, get the manifest, put it and convert");
+
+/**
+ * Fetch the HTML file using the URL in the input form `pep_url`, fetch or extract the manifest following
+ * the rules of the WP specification, display that manifest in the text box `wpm_holder`. Then
+ * calculate the canonical manifest and display it in `canonical_wpm` text box.
+ *
+ * @async
+ * @param {Event} e - Event structure (unused)
+ */
+async function get_pep(e) {
+    const logger = new Logger();
+    try {
+        const pep_url  = document.getElementById('pep_url').value;
+        const wpm_holder = document.getElementById('wpm_holder');
+        const canonical_wpm = document.getElementById('canonical_wpm');
+
+        const dom = await fetch_html(pep_url);
+        const manifest_data = await obtain_manifest(dom);
+
+        wpm_holder.value = manifest_data.manifest_text;
+
+        const manifest_object = get_canonical_manifest(logger, manifest_data);
+        canonical_wpm.value = JSON.stringify(manifest_object, null, 4);
+
+        document.getElementById('wpm_url').value = '';
+        document.getElementById('pep_url').value = '';
+    } catch (err) {
+        console.error(`Error in get_pep: ${err.message}`);
+        alert(`Error in getting the Primary Entry Page: ${err.message}`);
+    } finally {
+        console.log(logger.toString());
+    }
 }
 
-
-
+// Just a small goody for the home page...
+// eslint-disable-next-line no-unused-vars
+function printDate() {
+    const now = new Date(document.lastModified);
+    let textout = now.getDate() + ' ';
+    const month = now.getMonth();
+    if (month === 0) textout += 'January';
+    if (month === 1) textout += 'February';
+    if (month === 2) textout += 'March';
+    if (month === 3) textout += 'April';
+    if (month === 4) textout += 'May';
+    if (month === 5) textout += 'June';
+    if (month === 6) textout += 'July';
+    if (month === 7) textout += 'August';
+    if (month === 8) textout += 'September';
+    if (month === 9) textout += 'October';
+    if (month === 10) textout += 'November';
+    if (month === 11) textout += 'December';
+    textout += ', ' + now.getFullYear();
+    return textout;
+}
 
 window.addEventListener('load', (e) => {
     document.getElementById('fetch_wpm').addEventListener('click', get_wpm);
-    document.getElementById('upload_wpm').addEventListener('click', upload_wpm);
     document.getElementById('fetch_pep').addEventListener('click', get_pep);
     document.getElementById('canonicalize').addEventListener('click', convert);
+    document.getElementById('upload_wpm').addEventListener('change', upload_wpm);
+    document.getElementById('date').textContent = printDate();
 });
+
+
